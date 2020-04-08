aaf3826420fedeb83ccc28cb408b5aeee8eb0887
sandipk01 <sandipkengar01@gmail.com>
2020-04-09 02:49:54 +0530

    REFACTORED : added parameter as a type of scubscription

--- src/main/java/com/bridgelabz/cab/adapter/InvoiceFactory.java
+++ src/main/java/com/bridgelabz/cab/adapter/InvoiceFactory.java
@@ -3,8 +3,8 @@ package com.bridgelabz.cab.adapter;
 public class InvoiceFactory {
 
     //Method for getting instance of a invoice object
-    public static IInvoice getInvoiceInstance() {
-        IInvoice iInvoice = new Invoice();
+    public static IInvoice getInvoiceInstance(Invoice.TypeOfSubscription typeOfSubscription) {
+        IInvoice iInvoice = new Invoice(typeOfSubscription);
         return iInvoice;
     }
 }
