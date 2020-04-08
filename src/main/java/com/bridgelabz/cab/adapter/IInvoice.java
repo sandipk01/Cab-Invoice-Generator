f7a03a14d21f69b322e8520249b425a791d15f73
sandipk01 <sandipkengar01@gmail.com>
2020-04-09 02:19:14 +0530

    ADDED : addRides and getInvoiceDetals abstarct methods declared

--- src/main/java/com/bridgelabz/cab/adapter/IInvoice.java
+++ src/main/java/com/bridgelabz/cab/adapter/IInvoice.java
@@ -1,9 +1,15 @@
 package com.bridgelabz.cab.adapter;
 
+import com.bridgelabz.cab.model.InvoiceDetails;
 import com.bridgelabz.cab.model.Ride;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public interface IInvoice {
 
     double calculateFare(double distance,double time);
     double calculateFare(Ride[] rides);
+    Map<String, ArrayList<Ride>> addRides(String userId, Ride[] rides);
+    InvoiceDetails getInvoiceDetails(String userId, Map<String, ArrayList<Ride>> userRides);
 }
