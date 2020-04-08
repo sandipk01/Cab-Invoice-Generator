1ed2b21ca574bad47ea8760f1cddcfab335d8c92
sandipk01 <sandipkengar01@gmail.com>
2020-04-09 02:20:32 +0530

    ADDED : addRides and getInvoiceDetals abstarct methods invoked

--- src/main/java/com/bridgelabz/cab/service/CabBooking.java
+++ src/main/java/com/bridgelabz/cab/service/CabBooking.java
@@ -5,9 +5,14 @@ import com.bridgelabz.cab.model.InvoiceDetails;
 import com.bridgelabz.cab.adapter.InvoiceFactory;
 import com.bridgelabz.cab.model.Ride;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CabBooking {
 
     private IInvoice iInvoice;
+    private RideRepository rideRepository;
 
     //Method for calculating fare
     public double calculateRideFare(double distance, double time) {
@@ -20,6 +25,18 @@ public class CabBooking {
         iInvoice = InvoiceFactory.getInvoiceInstance();
         double totalFare=iInvoice.calculateFare(rides);
         return new InvoiceDetails(rides.length,totalFare);
+    }
+
+    //Method is invoking multiple rides added for particular user.
+    public Map<String, ArrayList<Ride>> addRides(String userId, Ride[] rides) {
+        iInvoice=InvoiceFactory.getInvoiceInstance();
+        return iInvoice.addRides(userId,rides);
+    }
+
+    //Method is invoking getting invoice details of particular user.
+    public InvoiceDetails getInvoiceDetails(String userId,Map<String, ArrayList<Ride>> userRides) {
+        iInvoice=InvoiceFactory.getInvoiceInstance();
+        return iInvoice.getInvoiceDetails(userId,userRides);
     }
 
 }
