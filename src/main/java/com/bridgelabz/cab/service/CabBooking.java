8ef71452e0bd590a0f837fffb1fc2e4e8bb795e1
sandipk01 <sandipkengar01@gmail.com>
2020-04-09 02:50:41 +0530

    ADDED : paremeterized constructor

--- src/main/java/com/bridgelabz/cab/service/CabBooking.java
+++ src/main/java/com/bridgelabz/cab/service/CabBooking.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.cab.service;
 
 import com.bridgelabz.cab.adapter.IInvoice;
+import com.bridgelabz.cab.adapter.Invoice;
 import com.bridgelabz.cab.model.InvoiceDetails;
 import com.bridgelabz.cab.adapter.InvoiceFactory;
 import com.bridgelabz.cab.model.Ride;
@@ -12,30 +13,29 @@ import java.util.Map;
 public class CabBooking {
 
     private IInvoice iInvoice;
-    private RideRepository rideRepository;
+
+    public CabBooking(Invoice.TypeOfSubscription typeOfSubscription){
+        iInvoice=InvoiceFactory.getInvoiceInstance(typeOfSubscription);
+    }
 
     //Method for calculating fare
     public double calculateRideFare(double distance, double time) {
-        iInvoice = InvoiceFactory.getInvoiceInstance();
         return iInvoice.calculateFare(distance, time);
     }
 
     //Method for calculating multiple fare and generating invoice details
     public InvoiceDetails calculateRideFare(Ride[] rides) {
-        iInvoice = InvoiceFactory.getInvoiceInstance();
         double totalFare=iInvoice.calculateFare(rides);
         return new InvoiceDetails(rides.length,totalFare);
     }
 
     //Method is invoking multiple rides added for particular user.
     public Map<String, ArrayList<Ride>> addRides(String userId, Ride[] rides) {
-        iInvoice=InvoiceFactory.getInvoiceInstance();
         return iInvoice.addRides(userId,rides);
     }
 
     //Method is invoking getting invoice details of particular user.
     public InvoiceDetails getInvoiceDetails(String userId,Map<String, ArrayList<Ride>> userRides) {
-        iInvoice=InvoiceFactory.getInvoiceInstance();
         return iInvoice.getInvoiceDetails(userId,userRides);
     }
 
