bbb38dcdc6f06bd379b09a27803c30995518c67a
sandipk01 <sandipkengar01@gmail.com>
2020-04-08 22:46:20 +0530

    ADDED : Test case for calculating fare should return total fare

---
+++ src/test/java/com/bridgelabz/testcab/TestCabBooking.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.testcab;
+
+import com.bridgelabz.cab.service.CabBooking;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestCabBooking {
+
+    private CabBooking cabBooking;
+
+    @Test
+    public void givenDistanceTime_WhenCalculateFare_ThenShouldReturnTotalFare() {
+        cabBooking = new CabBooking();
+        double totalFare = cabBooking.calculateRideFare(15, 30.15);
+        Assert.assertEquals(180.15, totalFare, 1);
+    }
+}
