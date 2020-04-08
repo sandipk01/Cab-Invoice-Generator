aed3cef41289fc8223385018103c0f64eb6d9d21
sandipk01 <sandipkengar01@gmail.com>
2020-04-08 23:25:27 +0530

    ADDED : Calculating fare for multiple rides method is invoked and return total fare

--- src/main/java/com/bridgelabz/cab/service/CabBooking.java
+++ src/main/java/com/bridgelabz/cab/service/CabBooking.java
@@ -2,12 +2,21 @@ package com.bridgelabz.cab.service;
 
 import com.bridgelabz.cab.adapter.IInvoice;
 import com.bridgelabz.cab.adapter.InvoiceFactory;
+import com.bridgelabz.cab.model.Ride;
 
 public class CabBooking {
 
+    private IInvoice iInvoice;
+
     //Method for calculating fare
     public double calculateRideFare(double distance, double time) {
-        IInvoice iInvoice= InvoiceFactory.getInvoiceInstance();
-        return iInvoice.calculateFare(distance,time);
+        iInvoice = InvoiceFactory.getInvoiceInstance();
+        return iInvoice.calculateFare(distance, time);
+    }
+
+    //Method for calculating multiple fare
+    public double calculateRideFare(Ride[] rides) {
+        iInvoice = InvoiceFactory.getInvoiceInstance();
+        return iInvoice.calculateFare(rides);
     }
 }
