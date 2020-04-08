78bf60496745fa0aa9a7a25c6aed37fdf5d21c7a
sandipk01 <sandipkengar01@gmail.com>
2020-04-08 22:37:07 +0530

    ADDED : Given distance and time should return total fare

--- src/main/java/com/bridgelabz/cab/adapter/Invoice.java
+++ src/main/java/com/bridgelabz/cab/adapter/Invoice.java
@@ -1,5 +1,17 @@
-package com.bridgelabz.cab.service;
+package com.bridgelabz.cab.adapter;
 
-public class Invoice {
+public class Invoice implements IInvoice {
+
+    //Constants
+    private double RATE_PER_KM = 10;
+    private int RATE_PER_MIN = 1;
+    private double MINIMUM_FARE = 5;
+
+    //Calculating Fare
+    @Override
+    public double calculateFare(double distance, double time) {
+        double totalFare = RATE_PER_KM * distance + RATE_PER_MIN * time;
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
 
 }
