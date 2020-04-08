a1775aeb961c9ab154ea5a790b457504ee80f9c9
sandipk01 <sandipkengar01@gmail.com>
2020-04-09 02:21:56 +0530

    ADDED : addRides and getInvoiceDetals logic is implemented

--- src/main/java/com/bridgelabz/cab/service/RideRepository.java
+++ src/main/java/com/bridgelabz/cab/service/RideRepository.java
@@ -1,4 +1,25 @@
 package com.bridgelabz.cab.service;
 
+import com.bridgelabz.cab.model.Ride;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class RideRepository {
+
+    //Method for adding rides logic
+    public Map<String,ArrayList<Ride>> addRides(String userId, Ride[] rides) {
+        Map<String, ArrayList<Ride>> userRides = new HashMap<>();
+        ArrayList<Ride> rideArrayList = userRides.get(userId);
+        if (rideArrayList == null)
+            userRides.put(userId, new ArrayList<>(Arrays.asList(rides)));
+        return userRides;
+    }
+
+    //Method for getting ride detail of particular user
+    public Ride[] getRidesByUserId(String userId,Map<String,ArrayList<Ride>> userRides) {
+       return userRides.get(userId).toArray(new Ride[0]);
+    }
 }
