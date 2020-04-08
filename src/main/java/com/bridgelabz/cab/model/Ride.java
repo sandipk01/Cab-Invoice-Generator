073649da57ece247239471744eada807681c5fcc
sandipk01 <sandipkengar01@gmail.com>
2020-04-08 23:23:49 +0530

    ADDED : Ride pojo class is added

--- src/main/java/com/bridgelabz/cab/model/Ride.java
+++ src/main/java/com/bridgelabz/cab/model/Ride.java
@@ -1,4 +1,19 @@
 package com.bridgelabz.cab.model;
 
 public class Ride {
+    private double time;
+    private double distance;
+
+    public Ride(double distance, double time) {
+        this.distance = distance;
+        this.time = time;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
 }
