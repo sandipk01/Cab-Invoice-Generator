ea1e5f51ee3f99b595e3aa8a5b24b13a637430e5
sandipk01 <sandipkengar01@gmail.com>
2020-04-08 22:43:43 +0530

    ADDED : getInvoiceInstance return object of Invoice class

--- src/main/java/com/bridgelabz/cab/adapter/InvoiceFactory.java
+++ src/main/java/com/bridgelabz/cab/adapter/InvoiceFactory.java
@@ -1,4 +1,10 @@
 package com.bridgelabz.cab.adapter;
 
 public class InvoiceFactory {
+
+    //Method for getting instance of a invoice object
+    public static IInvoice getInvoiceInstance() {
+        IInvoice iInvoice = new Invoice();
+        return iInvoice;
+    }
 }
