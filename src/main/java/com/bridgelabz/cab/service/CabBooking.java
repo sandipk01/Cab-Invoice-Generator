d2b848e83910a020820c579ec1e987d3e80a0c65
sandipk01 <sandipkengar01@gmail.com>
2020-04-08 22:45:08 +0530

    ADDED : Invokes calculateFare method returns total fare

--- src/main/java/com/bridgelabz/cab/service/CabBooking.java
+++ src/main/java/com/bridgelabz/cab/service/CabBooking.java
@@ -1,4 +1,13 @@
 package com.bridgelabz.cab.service;
 
+import com.bridgelabz.cab.adapter.IInvoice;
+import com.bridgelabz.cab.adapter.InvoiceFactory;
+
 public class CabBooking {
+
+    //Method for calculating fare
+    public double calculateRideFare(double distance, double time) {
+        IInvoice iInvoice= InvoiceFactory.getInvoiceInstance();
+        return iInvoice.calculateFare(distance,time);
+    }
 }
